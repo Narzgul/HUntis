bfe1ab7f09d99a1897f8bb9858aed41f68b3461d
Narzgul <tzoockeer@gmail.com>
2023-08-26 14:35:39 +0200

    fix time grid so weekdays line up (at least in my school)

--- lib/untis_api.dart
+++ lib/untis_api.dart
@@ -775,7 +775,7 @@ class Timegrid {
   );
   factory Timegrid._fromList(List<List<List<DayTime>>?> list) {
     return Timegrid._(
-        list[1], list[2], list[3], list[4], list[5], list[6], list[0]);
+        list[2], list[3], list[4], list[5], list[6], list[0], list[1]);
   }
 
   asList() {
